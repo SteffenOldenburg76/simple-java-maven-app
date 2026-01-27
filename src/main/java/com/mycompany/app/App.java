370fcb94041ae354a636b41bbc4cb17cd2502f2d
SteffenOldenburg76 <steffen.oldenburg@web.de>
2026-01-27 14:01:26 +0100

    Update App.java
    trigger codeql for sql

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -2,7 +2,9 @@ package com.mycompany.app;
 
 import java.util.Random;
 import javax.crypto.Cipher;
-    
+import java.sql.*;
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * Hello world!
  */
@@ -31,4 +33,13 @@ public class App {
     public String getMessage() {
         return MESSAGE;
     }
+
+    public void executeQuery(HttpServletRequest request, Connection conn) throws SQLException {
+        // TRIGGER: Directly concatenating unsanitized 'id' parameter into SQL
+        String id = request.getParameter("id");
+        String query = "SELECT * FROM users WHERE id = " + id;
+        
+        Statement statement = conn.createStatement();
+        ResultSet rs = statement.executeQuery(query);
+    }    
 }
