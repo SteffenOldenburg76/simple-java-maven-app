6c7edbd8ba2cda2637fe2a4f8364ff34f1ee2750
SteffenOldenburg76 <steffen.oldenburg@web.de>
2026-01-27 11:57:11 +0100

    Update deliver.sh
    set +- x

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
+set -x
 mvn jar:jar install:install help:evaluate -Dexpression=project.name
+set +x
 
+set -x
 NAME=$(mvn -q -DforceStdout help:evaluate -Dexpression=project.name)
+set +x
 echo "Name: $NAME"
 
+set -x
 VERSION=$(mvn -q -DforceStdout help:evaluate -Dexpression=project.version)
+set +x
 echo "Version: $VERSION"
 
+set -x
 java -jar target/"$NAME"-"$VERSION".jar
